321d8e982723a860d430070388b5075364b78fe6
FinleyCox <111890705+FinleyCox@users.noreply.github.com>
2025-04-18 11:51:52 +0900

    add speechToText

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:permission_handler/permission_handler.dart';
+// 音声設定参考：　https://hash-code.net/flutter/flutter-how-to-use-a-microphone-with-an-android-emulator/
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +10,114 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DETECT DOG',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 212, 119, 52)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePageDogName(title: 'DOG DETECT'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MyHomePageDogName extends StatefulWidget {
+  const MyHomePageDogName({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePageDogName> createState() => _MyHomePageDogNameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageDogNameState extends State<MyHomePageDogName> {
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _dogName = '';
+  String? _dogImage;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+    requestPermission();
+  }
+
+  Future<void> requestPermission() async {
+    var status = await Permission.microphone.status;
+    if(!status.isGranted) {
+      await Permission.microphone.request();
+    }
+  }
+
+  Future<void> startListening() async {
+    bool available = await _speech.initialize(
+      onStatus: (status) => print('Status: $status'),
+      onError: (error) => print("Error: $error"),
+      debugLogging: true,
+    );
+    if(available) {
+      setState(() {
+        _isListening = true;
+      });
+      _speech.listen(
+        onResult: (result) {
+          setState(() {
+            _dogName = result.recognizedWords;
+            _updateDogImage(_dogName);
+          });
+        }
+      );
+    }
+  }
+  void _updateDogImage(String name) {
+    String normalized = name.toLowerCase();
+    if(normalized.contains('りき') || normalized.contains('riki')) {
+      _dogImage = 'assets/images/riki.jpg';
+    } else if(normalized.contains('じゃっく') || normalized.contains('jack')) {
+      _dogImage = 'assets/images/jack.jpg';
+    } else {
+      _dogImage = null;
+    }
+  }
+
+  void stopListening() {
+    if(_speech.isListening) {
+      _speech.stop();
+      setState(() {
+        _isListening = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('認識された名前: $_dogName'),
+            const SizedBox(height: 20),
+            _dogImage != null ? Image.asset(_dogImage!, height: 200) : const Text('犬の画像が見つかりません'),
           ],
         ),
-      ),
+      ), 
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () async {
+          if(_isListening) {
+            stopListening();
+          } else if(await _speech.hasPermission && _speech.isNotListening) {
+              await startListening();
+          }
+        },
+        tooltip: 'Mic',
+        child: Icon(_isListening ? Icons.mic_off : Icons.mic),
+      ),
     );
   }
 }
